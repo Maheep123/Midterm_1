5126b86dcf3513d563af1c9eabc16ca5b5ca6f7a
Maheep123 <155857181+Maheep123@users.noreply.github.com>
2024-07-05 17:01:28 -0700

    Create Ans2.js

--- Ans2.js
+++ Ans2.js
@@ -1,1 +1,19 @@
+function twoSum(nums, target) {
+    const numMap = new Map();
 
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
+        }
+
+        numMap.set(nums[i], i);
+    }
+
+    return null;
+}
+
+console.log(twoSum([1, 5, 2, 7], 8));
+console.log(twoSum([20, 1, 5, 2, 11], 3));
+console.log(twoSum([3, 2, 4], 6));
